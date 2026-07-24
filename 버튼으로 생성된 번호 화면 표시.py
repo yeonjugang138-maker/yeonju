e0c6e7a3a07a599c5e857831c65d514354066a51
yeonju <yeonjugang138@gmail.com>
2026-07-24 16:44:53 +0900

    로또 번호 생성 프로그램

---
+++ 버튼으로 생성된 번호 화면 표시.py
@@ -0,0 +1,19 @@
+import tkinter
+import tkinter.font
+import random
+
+lotto_num = range(1,46)
+
+def buttonClick():
+    label = tkinter.Label(window, text=str(random.sample(lotto_num,6)))
+    label.pack()
+    
+window=tkinter.Tk()
+window.title("lotto")
+window.geometry("400x200")
+window.resizable(False, False)
+
+button = tkinter.Button(window, overrelief="solid",text="번호확인", width=15, command=buttonClick, repeatdelay=1000, repeatinterval=100)
+button.pack()
+
+window.mainloop()
